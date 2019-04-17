8bc787c1391b1ac3433c596c643555932df08764
ColbySaxton <cas264@case.edu>
2019-04-16 20:14:55 -0400

    complete string barricade

--- TreeRoot/src/typecheck/StringBarricade.java
+++ TreeRoot/src/typecheck/StringBarricade.java
@@ -1,42 +1,62 @@
 package typecheck;
 
+import java.util.ArrayList;
+
 public class StringBarricade implements Barricade {
 	private String input;
 	
-	private Object getType(String input) {
-		
+	private boolean isValidRule(String rule) {
+		ArrayList<String> operators = new ArrayList<String>();
+		boolean hasOperator = false;
+		operators.add("+");
+		operators.add("-");
+		operators.add("/");
+		operators.add("*");
+		operators.add("^");
+		for(int i = 0; i >= operators.size(); i++) {
+			CharSequence iterOp = operators.get(i);
+			if(rule.contains(iterOp)) {
+				hasOperator = true;
+			}
+		}
+		if(rule.startsWith("function") || (hasOperator == true)) {
+			return true;
+		}
+		return false;
 	}
 	
-	private String[] splitInput() {
-		
+	public boolean isValid(String thisinput) {
+		input = thisinput;
+		if(isEmpty()) {
+			return false;
+		}
+		String[] splitRuleReturn = splitRuleAndReturn();
+		if(!isValidSplit(splitRuleReturn) || !isValidRule(splitRuleReturn[0])) {
+			return false;
+		}
+		return true;
 	}
 	
-	private boolean isValidType() {
-		
-	}
-	
-	public boolean isValid(String input) {
-		
-	}
-	
-	public boolean isValidRuleOperation() {
-		
+	public boolean isValidSplit(String[] split) {
+		if(split.length != 2) {
+			return false;
+		}
+		return true;
 	}
 	
 	public boolean isEmpty() {
-		
+		if(input.length() == 0) {
+			return true;
+		}
+		return false;
 	}
 	
 	private String[] splitRuleAndReturn() {
 		String[] separateRuleReturn = null;
-		CharSequence divider = ":";
+		CharSequence divider = "=";
 		if(input.contains(divider)) {
-			separateRuleReturn = input.split("\\:");
+			separateRuleReturn = input.split("\\=");
 		}	
 		return separateRuleReturn;
-	}
-	
-	private boolean isValidReturn() {
-		
 	}
 }
