f8030e798b99d204b09fb6caf3d75d94c45170f7
Justin <jwen8545@gmail.com>
2019-04-17 11:38:47 -0400

    Update StringBarricade.java
    more comments

--- typeCheck/src/typecheck/StringBarricade.java
+++ typeCheck/src/typecheck/StringBarricade.java
@@ -9,11 +9,20 @@ public class StringBarricade implements Barricade {
 	private String input;
 	private List<String> allowedTypes;
 	
+	/**Initializes the barricade for Rules
+	 * 
+	 * @param types the types allowed in the tree
+	 */
 	public StringBarricade(List<String> types) {
 		input = "";
 		allowedTypes = types;
 	}
 	
+	/**Checks whether the rule contains an operator
+	 * or is a function
+	 * @param rule = the expression of the rule such as "Integer+Double"
+	 * @return whether the rule is valid or not
+	 */
 	private boolean isValidRule(String rule) {
 		List<CharSequence> operators = new ArrayList<CharSequence>();
 		boolean hasOperator = false;
@@ -30,19 +39,39 @@ public class StringBarricade implements Barricade {
 		return rule.startsWith("function") || hasOperator;
 	}
 	
+	/**Checks whether the input is valid
+	 * If it is not empty
+	 * is in the proper format with one "=" sign
+	 * is either a function or has an operator on the left half
+	 * has resolving type that is allowed
+	 * @param thisinput = the string line to be resolved
+	 * @return whether the line is a valid rule
+	 */
 	public boolean isValid(String thisinput) {
 		input = thisinput;
 		return !isEmpty() && isValidSplit(splitRuleAndReturn()) && isValidRule(splitRuleAndReturn()[0]) && allowedTypes.contains(splitRuleAndReturn()[1]);
 	}
 	
+	/**The rule line split by the "=" signs
+	 * if it is a valid rule it should be 2 parts
+	 * @param split = the number of parts to the rule
+	 * @return whether the line could be a rule
+	 */
 	private boolean isValidSplit(String[] split) {
 		return split.length == 2;
 	}
 	
+	/**Whether the line is empty
+	 * 
+	 */
 	public boolean isEmpty() {
 		return input.isEmpty();
 	}
 	
+	/**Splits the line into separate portions using "=" as a marker
+	 * also removes all whitespaces
+	 * @return the individual items in an array
+	 */
 	private String[] splitRuleAndReturn() {
 		List<String> separateRuleReturn = Stream.of(input.replaceAll("\\s+", "").split("="))
 				.map (elem -> new String(elem))
